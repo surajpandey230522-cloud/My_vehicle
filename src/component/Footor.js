e2c07a7955d167afb7281e32c4734f2810a69938
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-06 20:52:28 +0530

    jhkjhjkh

--- src/component/Footor.js
+++ src/component/Footor.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 function Footor() {
+let footerstyle={
+  Position:"absolute",
+  top:"100vh",
+  width:"100%"
+}
   return (
-    <div>
-      my work!
-    </div>
+    
+      <footer className="bg-dark text-light py-3" style={footerstyle}>
+
+        <p className='text-center'>
+          2026 Vehicle World&copy; vehicle.com
+          Designed with ❤️ using React
+        </p>
+
+    </footer>
   )
 }
 
