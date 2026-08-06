c87a9e509333605b77266139e4f5e1d3a970ea48
Suraj Pandey <surajpandey230522@gmail.com>
2026-08-06 20:09:30 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
 
       <ul className="nav-links">
        
-        <li><Link to="/">Hero</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">Services</Link></li>
         <li><Link to="/contact">Contact</Link></li>
